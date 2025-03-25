25aaa9be6f0a80b7778ad176c75dfd7c18b1932a
allysonschaedlerbrinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-03-25 08:38:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function VerificarSinal(numero){
+    if(numero > 0) {
+        console.log('positivo');
+    }else if(numero < 0){
+        console.log('negativo');
+    } else{
+        console.log('nulo')
+    }
+}
+
+function CoventerTemperatura(temperatura){
+    if(temperatura > 0){
+        console.log('Celsius')
+    } else {
+        console.log('fahrenhit')
+    }
+}
+
+
+let text = "Somátorio";
+let i = 0;
+while (i < 100) {
+  text += "<br>O número é" + i;
+  i++;
+}
+
+function SubstituirPalavras(string){
+    
+
+
+}
+
+function ParaNara(parameter1, parameter2, parameter3) {
+    // code to be executed
+  } 
