34124ae04f80999b7f61f660994a06a46d87c91f
allysonschaedlerbrinkerhoff <allyson_brinkerhoff@estudante.sesisenai.org.br>
2025-03-26 08:53:22 -0300

    UPDATE- Atv 21-24

--- script.js
+++ script.js
@@ -33,3 +33,31 @@ function SubstituirPalavras(string){
 function ParaNara(parameter1, parameter2, parameter3) {
     // code to be executed
   } 
+
+function SubstituirPalavras(){
+    console.log("Substituir palavra azul por Vermelho")
+    let texto= prompt ('digite uma frase ou texto com a palavra azul:')
+
+    console.log(texto.replace(/azul/g))
+
+}
+
+function atividade21(){
+
+    console.log("Soma dos Elementos de um Array");
+
+    let valores = prompt("Digite números separados na vírgula").split 
+    (",").map(Number);
+
+    console.log("Soma:", valores.reduce((acc, val) => acc+ val));
+    
+}
+
+function atividade24() {
+
+    console.log("Capitalizar a primeira letra");
+    let texto = prompt ("Digite uma palavra:");
+    console.log(texto.charAt(0).toUpperCase() + texto.slice(1));
+
+}
+
